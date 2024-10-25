e57b4dc4bf86ffa6f4fd833b0f1c78ebec0d9ee5
mdshowmik <39517713+mdshowmik@users.noreply.github.com>
2024-10-25 03:43:08 +0200

    command validity checked

--- src/Server.java
+++ src/Server.java
@@ -21,7 +21,6 @@ public class Server implements Runnable {
     private boolean isready;
     private boolean blocked = false;
     private CopyOnWriteArrayList<String> commandsStored = new CopyOnWriteArrayList<>();
-    private List<Server> servers;
 
 
     public Server(String name, int port) throws IOException {
@@ -35,7 +34,6 @@ public class Server implements Runnable {
         bindToPort(port);
         this.consensus = false;
         this.isready = true;
-        this.servers = new ArrayList<>();
     }
 
     //assigns port
